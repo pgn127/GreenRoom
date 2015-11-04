5cb3c925827171a16989402f5a9870849df07672
Daniel Del Core <daniel.delcore.92@gmail.com>
2015-11-04 15:34:51 +1100

    Added segues from list/fav table view controllers. Added fallback for no internet connection. Storyboard updates / restructure. Design Documentation + Presentation + Readme.txt

---
+++ assignment4/assignment4/CustomTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CustomTableViewCell.swift
+//  assignment4
+//
+//  Created by Daniel Del Core on 4/11/2015.
+//  Copyright © 2015 Daniel Del Core. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+    var locationId: Int?
+}
---
+++ assignment4/assignment4/Reachability.swift
@@ -0,0 +1,29 @@
+//
+//  Reachability.swift
+//  assignment4
+//
+//  Created by Daniel Del Core on 4/11/2015.
+//  Copyright © 2015 Daniel Del Core. All rights reserved.
+//
+//  Code found here: http://stackoverflow.com/questions/30743408/check-for-internet-conncetion-in-swift-2-ios-9
+
+import Foundation
+import SystemConfiguration
+
+public class Reachability {
+    class func isConnectedToNetwork() -> Bool {
+        var zeroAddress = sockaddr_in()
+        zeroAddress.sin_len = UInt8(sizeofValue(zeroAddress))
+        zeroAddress.sin_family = sa_family_t(AF_INET)
+        let defaultRouteReachability = withUnsafePointer(&zeroAddress) {
+            SCNetworkReachabilityCreateWithAddress(nil, UnsafePointer($0))
+        }
+        var flags = SCNetworkReachabilityFlags()
+        if !SCNetworkReachabilityGetFlags(defaultRouteReachability!, &flags) {
+            return false
+        }
+        let isReachable = (flags.rawValue & UInt32(kSCNetworkFlagsReachable)) != 0
+        let needsConnection = (flags.rawValue & UInt32(kSCNetworkFlagsConnectionRequired)) != 0
+        return (isReachable && !needsConnection)
+    }
+}
