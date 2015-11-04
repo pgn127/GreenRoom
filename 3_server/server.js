45902fe944e885c61390b56001dd3f9b2c1ef5a1
Daniel Del Core <daniel.delcore.92@gmail.com>
2015-11-04 17:15:00 +1100

    Added a new express server in the 3_server directory

--- 3_server/server.js
+++ 3_server/server.js
@@ -1,19 +1,25 @@
 var fs = require('fs');
 var express = require('express');
 var bodyParser = require('body-parser');
+var path = require('path');
 var app = express();
 
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 app.use(express.static('./.tmp'));
+app.enable('trust proxy');
 
-app.get('/api/PostStatusUpdate', function(req, res) {
+app.post('/api/PostStatusUpdate', function (req, res) {
     console.log('/api/PostStatusUpdate POST');
     console.log(req.body);
     res.json({
         "status": true,
         "message": "Open"
     });
+});
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
 });
 
 app.listen(1337, function() {
